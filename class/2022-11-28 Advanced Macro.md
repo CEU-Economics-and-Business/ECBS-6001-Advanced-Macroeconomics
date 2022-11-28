5a2d420feee85a8377aab3b14e60ac925b7e62f0
Miklós Koren <miklos.koren@gmail.com>
2022-11-28 18:54:12 +0100

    Add class 09

---
+++ class/2022-11-28 Advanced Macro.md
@@ -0,0 +1,74 @@
+### 1. Simulate realizations of MC [X]
+### Break until 14:28
+### 2. More general results about convergence
+Remember
+$$
+\pi_t = \mathbf P' \pi_{t-1} 
+$$
+Stability properties?
+
+1. Steady state: start there, stay there. This always exists, because every stochastic P has the eigenvalue 1 at least once.
+$$
+\pi_* = \mathbf P' \pi_* 
+$$
+2. Convergence:
+$$
+\pi_\infty = \lim_{t\to \infty} (\mathbf P')^t \pi_0
+$$
+	- does this exist?
+	- is it independent of $\pi_0$?
+	- is it the same as $\pi_*$?
+If so, we call it the ergodic distribution of $P$. **Not every MC is ergodic.**
+> TODO: check exact definition of "ergodic"
+
+Two sufficient (but not necessary) conditions for convergence to a unique steady state, $\pi_\infty = \pi_*$.
+1. $P_{ij}>0$ for all $i,j$.
+2. For some $T>1$, $[\mathbf P'^T]_{ij}>0$ for all $i,j$
+
+### Counterexample:
+The identity matrix is not ergodic, $\pi_\infty = \pi_0$.
+
+### Eigenvalues
+If all eigenvalues are $|\lambda_i| < 1$ for $i=2,...,K$ then the MC is ergodic and $\pi_\infty = \pi_*$.
+
+### 3. Parametric MCs
+Take $x_t$: number of emails in your inbox on day $t$. Given $x_t$, you may get a new email, or you may delete an email from your inbox with some probability.
+$$
+\Pr(x_t=j|x_{t-1} = i) = \begin{cases}
+\lambda & \text{if }j = i + 1\\
+\mu & \text{if }j= i - 1\\
+1-\lambda-\mu & \text{if } j=i\\
+0 & \text{otherwise}
+\end{cases}
+$$
+(plus some lower bound if you don't have emails)
+$$
+\Pr(x_t=j| x_{t-1}=0) = \begin{cases}
+\lambda & j=1\\
+1-\lambda & j=0\\
+0 & j>1
+\end{cases}
+$$
+
+States: $k=0, 1, 2, ...$ Transition matrix
+$$
+\mathbf P = \begin{bmatrix}
+1-\lambda & \lambda & 0 & 0 &...\\
+\mu & 1-\lambda-\mu & \lambda & 0 & ...\\
+0 & \mu & 1-\lambda - \mu & \lambda & ...\\
+...
+\end{bmatrix}
+$$
+What is the steady state of this process?
+$$
+\pi_*' = \pi_*'\mathbf P
+$$
+$$
+\pi_{*0} = (1-\lambda)\pi_{*0} + \mu \pi_{*1}
+$$
+$$
+\pi_{*1} = \lambda \pi_{*0} + (1-\lambda-\mu)\pi_{*1} + \mu \pi_{*2}
+$$
+resulting in a second-order difference equation for $\pi_{*n}$. I can solve for this recursively.
+
+### 4. Start with dynamic programming
---
+++ class/class09.jl
@@ -0,0 +1,150 @@
+### A Pluto.jl notebook ###
+# v0.19.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 02dbf68a-6f1a-11ed-3415-93ae3f6181eb
+begin
+    import Pkg
+    # activate the shared project environment
+    Pkg.activate(".")
+    # instantiate, i.e. make sure that all packages are downloaded
+    Pkg.instantiate()
+end
+
+# ╔═╡ e7cf1910-0817-4a62-ad88-8ee858a64361
+using LinearAlgebra
+
+# ╔═╡ 636004ad-0804-4e4d-b908-c66183727479
+using Plots
+
+# ╔═╡ 913845fb-5281-4ccb-8044-a589601dc19b
+using Distributions
+
+# ╔═╡ fa4945c1-b586-41d1-961b-0db262af4da8
+eigen([1 0; 0 1])
+
+# ╔═╡ 2e698b34-5088-4cca-9614-c01236b6085f
+eigen([0 1; 1 0])
+
+# ╔═╡ b51ad58a-d7f2-4d5e-af2e-99385ba2b90f
+function simulate(P::Matrix{Float64}, x0::Int64, T=30)::Vector{Int64}
+	x = Vector{Int64}(undef, T)
+	x[1] = x0
+	for t=2:T
+		π = P[x[t-1], :]
+		x[t] = rand(Categorical(π))
+	end
+	return x
+end
+
+# ╔═╡ 759ee4a4-e0de-43de-b69f-b23e44f4bdea
+coin_flips = simulate([0.5 0.5; 0.5 0.5], 1, 300)
+
+# ╔═╡ dca51138-3768-4af6-ab82-4aa4feefba49
+mean(coin_flips)
+
+# ╔═╡ 0e8ff03b-49c0-467e-958b-4db4dd0aa43c
+plot(coin_flips)
+
+# ╔═╡ 7598d7f2-9920-4514-bc9c-d23e1056984f
+persistent_mc = simulate([0.9 0.1; 0.4 0.6], 1, 300)
+
+# ╔═╡ 62cb8ec8-4a09-4a22-9e24-d6c6ceb04daf
+plot(persistent_mc)
+
+# ╔═╡ f231e642-ab40-43d7-a859-59f8bb5f190c
+begin
+	P3=[0.9 0.1 0; 
+		0.15 0.7 0.15; 
+		0.0 0 1.0]
+	three_state_mc = simulate(P3, 1, 100)
+end
+
+# ╔═╡ 857765e4-60f4-4a53-b8e1-58007469f3cf
+plot(three_state_mc)
+
+# ╔═╡ 6ffbcc1d-8fc1-458e-bf0d-397696267816
+eigen(P3').values
+
+# ╔═╡ 8e6583e5-9d01-49d9-994a-bd91900cda82
+eigen(P3').vectors
+
+# ╔═╡ b5b9c867-43c3-4e53-8113-33bb75ee65de
+P3'
+
+# ╔═╡ 1aca0a03-dd40-4278-bfb5-be8bb1a6a97f
+(P3')^2
+
+# ╔═╡ 04c3a354-695d-41fc-b640-d9ec3675ec5a
+(P3')^30
+
+# ╔═╡ 10c56515-09ed-4726-8eb5-080396d09d66
+
+
+# ╔═╡ 792a87b8-5c0b-46b9-95e6-75c171ffa216
+coin_flip = Categorical([0.5, 0.5])
+
+# ╔═╡ 905c8437-dff2-45e7-ad03-7dff69621f43
+mean(coin_flip)
+
+# ╔═╡ 6db7c20b-4b95-4708-833a-d08953901190
+probs(coin_flip)
+
+# ╔═╡ 6b5d58c0-a6b3-488e-ab7e-aa18e203e92f
+mean(coin_flip)
+
+# ╔═╡ 4719706d-6c80-4f72-8c3c-f217a757f3e4
+rand(coin_flip)
+
+# ╔═╡ 0efc83c4-8064-4b40-b8ed-a225424971e2
+standard_normal = Normal(0.0, 1.0)
+
+# ╔═╡ 3e1e0e9c-8846-4f0d-bee7-f7dcbbe1edc9
+probs(standard_normal)
+
+# ╔═╡ b3e54a06-bbd6-4663-bc06-364c9c85470b
+mean(standard_normal)
+
+# ╔═╡ 6b21fb20-982a-4d69-92ba-660ae5f216c2
+rand(standard_normal)
+
+# ╔═╡ 3048a4c1-c688-4db4-b3a0-1bbabdf6c553
+
+
+# ╔═╡ 404b5556-f758-4df0-9a52-780862e09177
+
+
+# ╔═╡ Cell order:
+# ╠═02dbf68a-6f1a-11ed-3415-93ae3f6181eb
+# ╠═e7cf1910-0817-4a62-ad88-8ee858a64361
+# ╠═fa4945c1-b586-41d1-961b-0db262af4da8
+# ╠═2e698b34-5088-4cca-9614-c01236b6085f
+# ╠═b51ad58a-d7f2-4d5e-af2e-99385ba2b90f
+# ╠═759ee4a4-e0de-43de-b69f-b23e44f4bdea
+# ╠═dca51138-3768-4af6-ab82-4aa4feefba49
+# ╠═905c8437-dff2-45e7-ad03-7dff69621f43
+# ╠═636004ad-0804-4e4d-b908-c66183727479
+# ╠═0e8ff03b-49c0-467e-958b-4db4dd0aa43c
+# ╠═7598d7f2-9920-4514-bc9c-d23e1056984f
+# ╠═62cb8ec8-4a09-4a22-9e24-d6c6ceb04daf
+# ╠═f231e642-ab40-43d7-a859-59f8bb5f190c
+# ╠═857765e4-60f4-4a53-b8e1-58007469f3cf
+# ╠═6ffbcc1d-8fc1-458e-bf0d-397696267816
+# ╠═8e6583e5-9d01-49d9-994a-bd91900cda82
+# ╠═b5b9c867-43c3-4e53-8113-33bb75ee65de
+# ╠═1aca0a03-dd40-4278-bfb5-be8bb1a6a97f
+# ╠═04c3a354-695d-41fc-b640-d9ec3675ec5a
+# ╠═10c56515-09ed-4726-8eb5-080396d09d66
+# ╠═913845fb-5281-4ccb-8044-a589601dc19b
+# ╠═792a87b8-5c0b-46b9-95e6-75c171ffa216
+# ╠═6db7c20b-4b95-4708-833a-d08953901190
+# ╠═6b5d58c0-a6b3-488e-ab7e-aa18e203e92f
+# ╠═4719706d-6c80-4f72-8c3c-f217a757f3e4
+# ╠═0efc83c4-8064-4b40-b8ed-a225424971e2
+# ╠═3e1e0e9c-8846-4f0d-bee7-f7dcbbe1edc9
+# ╠═b3e54a06-bbd6-4663-bc06-364c9c85470b
+# ╠═6b21fb20-982a-4d69-92ba-660ae5f216c2
+# ╠═3048a4c1-c688-4db4-b3a0-1bbabdf6c553
+# ╠═404b5556-f758-4df0-9a52-780862e09177
